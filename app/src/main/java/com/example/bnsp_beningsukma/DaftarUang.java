5d81b975445475926fd9e8a773670b4b11344d17
beningsukmaningrum <54787621+beningsukmaningrum@users.noreply.github.com>
2022-09-22 21:14:20 +0700

    first

---
+++ app/src/main/java/com/example/bnsp_beningsukma/DaftarUang.java
@@ -0,0 +1,14 @@
+package com.example.bnsp_beningsukma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DaftarUang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar_uang);
+    }
+}
